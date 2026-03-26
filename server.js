e9aeea69a8924f179c91f461dafef6680a59e587
nandish3204 <168513872+nandish3204@users.noreply.github.com>
2026-03-26 13:05:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
 const port = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
-    fs.readFile('index.html', (err, data) => {
+    let filePath = '.' + req.url;
+
+    if (filePath === './') {
+        filePath = './index.html';
+    }
+
+    const extname = path.extname(filePath);
+
+    let contentType = 'text/html';
+    if (extname === '.css') contentType = 'text/css';
+    if (extname === '.js') contentType = 'text/javascript';
+
+    fs.readFile(filePath, (err, content) => {
         if (err) {
-            res.writeHead(500);
-            res.end("Error loading file");
+            res.writeHead(404);
+            res.end('File not found');
         } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data);
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content);
         }
     });
+
 }).listen(port, () => {
-    console.log("Server running on port " + port);
+    console.log(`Server running on port ${port}`);
 });
