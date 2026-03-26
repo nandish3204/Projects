ed0b27221f8a9ebe35134b4b720b25685d08cf7d
nandish3204 <168513872+nandish3204@users.noreply.github.com>
2026-03-26 12:53:41 +0530

    Implement basic HTTP server to serve index.html

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+    fs.readFile('index.html', (err, data) => {
+        if (err) {
+            res.writeHead(500);
+            res.end("Error loading file");
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        }
+    });
+}).listen(port, () => {
+    console.log("Server running on port " + port);
+});
